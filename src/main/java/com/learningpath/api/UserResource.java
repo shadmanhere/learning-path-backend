26bcc93e3e74fe79f4d4fe13e90bbb32071041d0
Shadman Ali <shadman.ali@live.co.uk>
2022-12-05 13:27:13 +0530

    installs spring-data-jpa, mysql-connector-java and lombok, creates api, user and role models, repo and services

---
+++ src/main/java/com/learningpath/api/UserResource.java
@@ -0,0 +1,23 @@
+package com.learningpath.api;
+
+import com.learningpath.model.User;
+import com.learningpath.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserResource {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>>getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+}
---
+++ src/main/java/com/learningpath/service/UserService.java
@@ -0,0 +1,14 @@
+package com.learningpath.service;
+
+import com.learningpath.model.Role;
+import com.learningpath.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    User getUser(String username);
+    List<User> getUsers();
+}
